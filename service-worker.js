aecd61b00239764b2b078db2aeaa2e8aeed2c798
smartbanksoal <endrastodwi79@gmail.com>
2026-03-02 08:41:18 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kuis-kelas-5-v1';
+const CACHE_NAME = 'kuis-kelas-5-v2';
 const urlsToCache = [
  './index.html',
   './icon-512.png',
@@ -16,61 +16,61 @@ const urlsToCache = [
   './soal/MTK-K5-S2-B6-HOTS.html',
   './soal/MTK-K5-S2-B7-HOTS.html',
   './soal/MTK-K5-S2-B8-HOTS.html',
-  './soal/gbr/email-format.jpg',
-  './soal/gbr/infografis-fakta.jpg',
-  './soal/gbr/poster-akronim.jpg',
-  './soal/gbr/siklus-hujan.jpg',
-  './soal/gbr/berita-opini.jpg',
-  './soal/gbr/soal5_plang_jalan.jpg',
-  './soal/gbr/soal10_kihajar.jpg',
-  './soal/gbr/soal15_struk.jpg',
-  './soal/gbr/soal20_rambu.jpg',
-  './soal/gbr/soal25_pengumuman.jpg',
-  './soal/gbr/gambar_soal3_sampah.jpg',
-  './soal/gbr/gambar_soal8_asappabrik.jpg',
-  './soal/gbr/gambar_soal12_bersihpantai.jpg',
-  './soal/gbr/gambar_soal17_panel_surya.jpg',
-  './soal/gbr/gambar_soal23_siklus_air.jpg',
-  './soal/gbr/gambar_buang_sampah_sungai.jpg',
-  './soal/gbr/gambar_gotong_royong.jpg',
-  './soal/gbr/gambar_hemat_air.jpg',
-  './soal/gbr/gambar_hutan_gundul.jpg',
-  './soal/gbr/gambar_pelari_tercepat.jpg',
-  './soal/gbr/gambar_sistem_pencernaan.jpg',
-  './soal/gbr/gambar_orang_berlari.jpg',
-  './soal/gbr/gambar_paru_paru.jpg',
-  './soal/gbr/gambar_piramida_makanan.jpg',
-  './soal/gbr/gambar_siklus_pertumbuhan.jpg',
-  './soal/gbr/garis_wallace.jpg',
-  './soal/gbr/hutan_bakau.jpg',
-  './soal/gbr/panel_surya.jpg',
-  './soal/gbr/peta_angin.jpg',
-  './soal/gbr/reboisasi.jpg',
-  './soal/gbr/soal_16.jpg',
-  './soal/gbr/soal_17.jpg',
-  './soal/gbr/soal_18.jpg',
-  './soal/gbr/soal_19.jpg',
-  './soal/gbr/soal_20.jpg',
-  './soal/gbr/hutan_gundul.jpg',
-  './soal/gbr/asap_kendaraan.jpg',
-  './soal/gbr/tanah_longsor.jpg',
-  './soal/gbr/es_kutub.jpg',
-  './soal/gbr/sungai_tercemar.jpg',
-  './soal/gbr/jam_0400.jpg',
-  './soal/gbr/busur_derajat.jpg',
-  './soal/gbr/atap_rumah.jpg',
-  './soal/gbr/gunting_terbuka.jpg',
-  './soal/gbr/sudut_meja.jpg',
-  './soal/gbr/soal_5.jpg',
-  './soal/gbr/soal_1.jpg',
-  './soal/gbr/soal_2.jpg',
-  './soal/gbr/soal_3.jpg',
-  './soal/gbr/soal_4.jpg',
-  './soal/gbr/gambar_piktogram_panen.jpg',
-  './soal/gbr/gambar_diagram_batang_ganda.jpg',
-  './soal/gbr/gambar_diagram_horizontal.jpg',
-  './soal/gbr/gambar_analisis_piktogram.jpg',
-  './soal/gbr/gambar_tabel_frekuensi.jpg'
+ './soal/gambar/email-format.jpg',
+  './soal/gambar/infografis-fakta.jpg',
+  './soal/gambar/poster-akronim.jpg',
+  './soal/gambar/siklus-hujan.jpg',
+  './soal/gambar/berita-opini.jpg',
+  './soal/gambar/soal5_plang_jalan.jpg',
+  './soal/gambar/soal10_kihajar.jpg',
+  './soal/gambar/soal15_struk.jpg',
+  './soal/gambar/soal20_rambu.jpg',
+  './soal/gambar/soal25_pengumuman.jpg',
+  './soal/gambar/gambar_soal3_sampah.jpg',
+  './soal/gambar/gambar_soal8_asappabrik.jpg',
+  './soal/gambar/gambar_soal12_bersihpantai.jpg',
+  './soal/gambar/gambar_soal17_panel_surya.jpg',
+  './soal/gambar/gambar_soal23_siklus_air.jpg',
+  './soal/gambar/gambar_buang_sampah_sungai.jpg',
+  './soal/gambar/gambar_gotong_royong.jpg',
+  './soal/gambar/gambar_hemat_air.jpg',
+  './soal/gambar/gambar_hutan_gundul.jpg',
+  './soal/gambar/gambar_pelari_tercepat.jpg',
+  './soal/gambar/gambar_sistem_pencernaan.jpg',
+  './soal/gambar/gambar_orang_berlari.jpg',
+  './soal/gambar/gambar_paru_paru.jpg',
+  './soal/gambar/gambar_piramida_makanan.jpg',
+  './soal/gambar/gambar_siklus_pertumbuhan.jpg',
+  './soal/gambar/garis_wallace.jpg',
+  './soal/gambar/hutan_bakau.jpg',
+  './soal/gambar/panel_surya.jpg',
+  './soal/gambar/peta_angin.jpg',
+  './soal/gambar/reboisasi.jpg',
+  './soal/gambar/soal_16.jpg',
+  './soal/gambar/soal_17.jpg',
+  './soal/gambar/soal_18.jpg',
+  './soal/gambar/soal_19.jpg',
+  './soal/gambar/soal_20.jpg',
+  './soal/gambar/hutan_gundul.jpg',
+  './soal/gambar/asap_kendaraan.jpg',
+  './soal/gambar/tanah_longsor.jpg',
+  './soal/gambar/es_kutub.jpg',
+  './soal/gambar/sungai_tercemar.jpg',
+  './soal/gambar/jam_0400.jpg',
+  './soal/gambar/busur_derajat.jpg',
+  './soal/gambar/atap_rumah.jpg',
+  './soal/gambar/gunting_terbuka.jpg',
+  './soal/gambar/sudut_meja.jpg',
+  './soal/gambar/soal_5.jpg',
+  './soal/gambar/soal_1.jpg',
+  './soal/gambar/soal_2.jpg',
+  './soal/gambar/soal_3.jpg',
+  './soal/gambar/soal_4.jpg',
+  './soal/gambar/gambar_piktogram_panen.jpg',
+  './soal/gambar/gambar_diagram_batang_ganda.jpg',
+  './soal/gambar/gambar_diagram_horizontal.jpg',
+  './soal/gambar/gambar_analisis_piktogram.jpg',
+  './soal/gambar/gambar_tabel_frekuensi.jpg'
 ];
 
 // Menyimpan file ke cache saat aplikasi pertama kali dibuka
@@ -95,4 +95,19 @@ self.addEventListener('fetch', event => {
       })
   );
 
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          // Jika nama cache di browser tidak sama dengan cacheName baru, hapus!
+          if (cache !== cacheName) {
+            console.log('Menghapus cache lama yang foldernya salah...');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
